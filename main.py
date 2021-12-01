ecd2f2512f4687d074b216514b9e4f846a33f211
Bhavy <bhavy2908@gmail.com>
2021-12-01 19:39:20 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route('/alris_signup/<string:inpt_acc_no>')
+def alris_ie(inpt_acc_no):
+    inpt = str(inpt_acc_no)
+    inpt_arr = inpt.split("_")
+    usrname = int(inpt_arr[0])
+    passwrd = int(inpt_arr[1])
+    if usrname - passwrd == 987650000:
+        return "yes"
+    else:
+        return "no"
+
+    
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
