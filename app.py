6ef5eaa1058b5de84cdbf985d78b8b85860d7ca3
Bhavy <bhavy2908@gmail.com>
2021-12-03 00:13:20 +0530

    deploy

--- app.py
+++ app.py
@@ -1,28 +1,40 @@
-from flask import Flask, jsonify
+from flask import Flask, request
+import jsonify as jf
+import pandas as pd
+
 app = Flask(__name__)
+df = pd.read_csv('data.csv')
+df2 = pd.read_csv('res.csv')
 
-@app.route('/signin/<string:inpt_acc_no>')
-def alris_ie(inpt_acc_no):
-    inpt = inpt_acc_no
-    inpt_arr = inpt.split("_")
-    usrname = int(inpt_arr[0])
-    passwrd = int(inpt_arr[1])
-    if usrname - passwrd == 987650000:
-        dict = {0 : "yes"}
-    else:
-        dict = {0 : "no"}
-    response = app.response_class(
-        response=json.dumps(dict),
-        status=200,
-        mimetype='application/json'
-    )
-    return response 
+def data(acc_number):
+    cur_df = df.loc[df['Account Number'] == acc_number].reset_index()
+    cur2_df = df2.loc[df2['account number'] == acc_number]
+    factor = cur2_df['FACTOR'].to_numpy()[0]
+    invest_to = cur2_df['Invest to'].to_numpy()[0]
+    bal = cur_df['Savings'].sum()
+    avg_exp = cur_df['Expenditure'].sum()/24
+    avg_inc = cur_df['Income'].sum()/24
+    name = cur_df['Name'].iloc[0]
+    cur_df = cur_df.drop(range(19))
+    inc = cur_df['Income'].to_numpy()
+    exp = cur_df['Expenditure'].to_numpy()
+    sav = cur_df['Savings'].to_numpy()
+    return {
+        "inc": list(inc),
+        "exp": list(exp),
+        "sav": list(sav),
+        "bal": bal,
+        "name": name,
+        "exptd_inc": avg_inc,
+        "exptd_exp": avg_exp,
+        "invest_to": invest_to,
+        "ati": bal*0.08/(1+factor),
+        "exptd_ret": bal*0.08,
+    }
 
-
-@app.route('/')
-def alris():
-    return "Welcome to alris' API"
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/", methods = ['POST', 'GET'])
+def function():
+    if request.method == 'POST':
+        acc_no = request.form['acc_no']
+        return jf(data(acc_no))
+    return "<p>Hello, World!</p>"
