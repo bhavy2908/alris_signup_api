c8829ab418637475edd270ad75ed20ae2f3f0103
Bhavy <bhavy2908@gmail.com>
2021-12-02 21:38:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
 from flask import Flask, jsonify
-from werkzeug.wrappers import request
 app = Flask(__name__)
 
-@app.route('/signin/<string:inpt_acc_no>', methods = ['POST'])
+@app.route('/signin/<string:inpt_acc_no>')
 def alris_ie(inpt_acc_no):
-    if request.method == 'POST':
-        inpt = inpt_acc_no
-        inpt_arr = inpt.split("_")
-        usrname = int(inpt_arr[0])
-        passwrd = int(inpt_arr[1])
+    inpt = inpt_acc_no
+    inpt_arr = inpt.split("_")
+    usrname = int(inpt_arr[0])
+    passwrd = int(inpt_arr[1])
+    string = "no"
+    if usrname - passwrd == 987650000:
+        string = "yes"
+    else:
         string = "no"
-        if usrname - passwrd == 987650000:
-            string = "yes"
-        else:
-            string = "no"
-        print(request.get_json())
-        return jsonify(string) 
+    return jsonify(string) 
 
 
 @app.route('/')
