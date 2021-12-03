e058c0aa57ba7062c21f2ea3e83cbb9aa0134920
Bhavy <bhavy2908@gmail.com>
2021-12-03 12:39:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,6 +36,6 @@ def data(acc_number):
 @app.route("/", methods = ['POST', 'GET'])
 def function():
     if request.method == 'POST':
-        acc_no = request.form['acc_no']
+        acc_no = request.data['acc_no']
         return jsonify(str(data(int(acc_no))))
     return "<p>Hello, World!</p>"
